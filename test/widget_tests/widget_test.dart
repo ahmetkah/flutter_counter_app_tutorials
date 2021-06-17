93d5143ef0b55883ac0a46158b148cfee2d2e1c1
“Ahmet <ahmet.kahrimanoglu@fstek.com>
2021-06-17 21:11:11 +0300

    widget test group içine alındı.

--- test/widget_tests/widget_test.dart
+++ test/widget_tests/widget_test.dart
@@ -9,99 +9,106 @@ Widget createCounterView() => MaterialApp(
     );
 
 void main() {
-  testWidgets(
-    'Sayaç Arttırma Testi',
-    (WidgetTester tester) async {
-      /// [pumpWidget]: Test edilecek Widget'ı oluşturur.
-      /// Yani widget ağacına ekler ve build() metodunu çağırır.
-      await tester.pumpWidget(const App());
+  group('Counter View Widget Tests |s', () {
+    testWidgets(
+      'Sayaç Arttırma Testi',
+      (WidgetTester tester) async {
+        /// [pumpWidget]: Test edilecek Widget'ı oluşturur.
+        /// Yani widget ağacına ekler ve build() metodunu çağırır.
+        await tester.pumpWidget(const App());
 
-      /// 2. Alternatif:
-      ///  await tester.pumpWidget(createCounterView());
+        /// 2. Alternatif:
+        ///  await tester.pumpWidget(createCounterView());
 
-      /// Find Fonksiyonu: Ara ve Bul
-      /// [find.text()]: Text widgetlerini bulmamızı sağlar.
-      /// [find.byIcon()]: Icon widgetlerini bulmamızı sağlar.
-      // Ekranda '0' içeren text widgetini arar
-      final _finder0 = find.text('0');
-      // Ekranda '1' içeren text widgetini arar
-      final _finder1 = find.text('1');
-      // Ekranda 'Add' ikon widgetini arar
-      final _finderIconAdd = find.byIcon(Icons.add);
+        /// Find Fonksiyonu: Ara ve Bul
+        /// [find.text()]: Text widgetlerini bulmamızı sağlar.
+        /// [find.byIcon()]: Icon widgetlerini bulmamızı sağlar.
+        // Ekranda '0' içeren text widgetini arar
+        final _finder0 = find.text('0');
+        // Ekranda '1' içeren text widgetini arar
+        final _finder1 = find.text('1');
+        // Ekranda 'Add' ikon widgetini arar
+        final _finderIconAdd = find.byIcon(Icons.add);
 
-      /// [findsOneWidget]: Ekranda o widgetın olduğunu doğrular.
-      expect(_finder0, findsOneWidget);
+        /// [findsOneWidget]: Ekranda o widgetın olduğunu doğrular.
+        expect(_finder0, findsOneWidget);
 
-      /// [findsNothing]: Ekranda o widget'ın olmadığını doğrular.
-      expect(_finder1, findsNothing);
+        /// [findsNothing]: Ekranda o widget'ın olmadığını doğrular.
+        expect(_finder1, findsNothing);
 
-      /// Widget Etkileşimleri: Widget'la etkileşime girer.
-      /// [tap()]: Bir widgete tıklanma/basma davranışını taklit eder.
-      await tester.tap(_finderIconAdd);
+        /// Widget Etkileşimleri: Widget'la etkileşime girer.
+        /// [tap()]: Bir widgete tıklanma/basma davranışını taklit eder.
+        await tester.tap(_finderIconAdd);
 
-      /// Rebuild:
-      /// [pump()]: Widget’ın re-build edilmesini sağlar.
-      await tester.pump();
+        /// Rebuild:
+        /// [pump()]: Widget’ın re-build edilmesini sağlar.
+        await tester.pump();
 
-      /// Ekranda sayaç değerinin "0" olmadığını doğruluyoruz
-      expect(_finder0, findsNothing);
+        /// [pumpAndSettle]: Widget’ın animasyonlar bittikten sonra
+        /// re-build edilmesini sağlar.
+        /// await tester.pumpAndSettle(const Duration(seconds: 1));
 
-      /// Ekranda sayaç değerinin "1" olduğunu doğruluyoruz
-      expect(_finder1, findsOneWidget);
-    },
-  );
+        /// Ekranda sayaç değerinin "0" olmadığını doğruluyoruz
+        expect(_finder0, findsNothing);
 
-  testWidgets(
-    'Sayaç Sıfırlama Testi',
-    (WidgetTester tester) async {
-      final _finder0 = find.text('0');
-      final _finder1 = find.text('1');
-      final _finder2 = find.text('2');
-      final _finderIconAdd = find.byIcon(Icons.add);
-      final _finderIconZero = find.byIcon(Icons.exposure_zero_sharp);
+        /// Ekranda sayaç değerinin "1" olduğunu doğruluyoruz
+        expect(_finder1, findsOneWidget);
+      },
+    );
 
-      await tester.pumpWidget(const App());
+    testWidgets(
+      'Sayaç Sıfırlama Testi',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(const App());
 
-      expect(_finder0, findsOneWidget);
+        final _finder0 = find.text('0');
+        final _finder1 = find.text('1');
+        final _finder2 = find.text('2');
+        final _finderIconAdd = find.byIcon(Icons.add);
+        final _finderIconZero = find.byIcon(Icons.exposure_zero_sharp);
+        //
+        expect(_finder0, findsOneWidget);
+        //
+        expect(_finder1, findsNothing);
+        //
+        expect(_finder2, findsNothing);
 
-      expect(_finder1, findsNothing);
-      expect(_finder2, findsNothing);
+        await tester.tap(_finderIconAdd);
+        await tester.tap(_finderIconAdd);
+        await tester.tap(_finderIconZero);
 
-      await tester.tap(_finderIconAdd);
-      await tester.tap(_finderIconAdd);
-      await tester.tap(_finderIconZero);
+        await tester.pump();
 
-      await tester.pump();
+        expect(_finder2, findsNothing);
 
-      expect(_finder2, findsNothing);
+        expect(_finder0, findsOneWidget);
+      },
+    );
 
-      expect(_finder0, findsOneWidget);
-    },
-  );
+    testWidgets(
+      'Sayaç Azaltma Testi',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(createCounterView());
 
-  testWidgets(
-    'Sayaç Azaltma Testi',
-    (WidgetTester tester) async {
-      await tester.pumpWidget(createCounterView());
+        final _finder0 = find.text('0');
+        final _finderMinus1 = find.text('-1');
+        final _finderIconRemove = find.byIcon(Icons.remove);
 
-      final _finder0 = find.text('0');
-      final _finderMinus1 = find.text('-1');
-      final _finderIconRemove = find.byIcon(Icons.remove);
+        expect(_finder0, findsOneWidget);
 
-      expect(_finder0, findsOneWidget);
+        expect(_finderMinus1, findsNothing);
 
-      expect(_finderMinus1, findsNothing);
+        await tester.tap(_finderIconRemove);
 
-      await tester.tap(_finderIconRemove);
+        await tester.pump();
 
-      await tester.pump();
+        expect(_finder0, findsNothing);
 
-      expect(_finder0, findsNothing);
-
-      expect(_finderMinus1, findsOneWidget);
-    },
-  );
+        expect(_finderMinus1, findsOneWidget);
+      },
+    );
+  });
 }
 
 /// Emülatör üzerinden testi takip etmek
-/// flutter run test/widget_tests/widget_test.dart
+/// flutter run test/widget_tests/counter_view_test.dart
