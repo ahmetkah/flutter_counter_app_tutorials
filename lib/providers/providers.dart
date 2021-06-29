14881c381bf027b54805707b2d6a3c9153b69322
“Ahmet <ahmet.kahrimanoglu@fstek.com>
2021-06-29 14:24:10 +0300

    second commit

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -2,7 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../notifiers/counter_state_notifier.dart';
 
-/// [3. Adım]: CounterNotifier sınıfını çağıran genel bir değişken tanımla.
+/// [3. Adım]: CounterNotifier sınıfını çağıran
+/// StateNotifierProvider için global bir değişken tanımla.
 final counterProvider = StateNotifierProvider<CounterStateNotifier, int>(
   (ref) => CounterStateNotifier(),
 );
