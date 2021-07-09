0a89e664687c75144c970be8b4bad2aa6a96d1c4
“Ahmet <ahmet.kahrimanoglu@fstek.com>
2021-07-09 22:42:15 +0300

    HydratedMixin ile son counter değeri kalıcı hale getirildi

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,8 +1,9 @@
 /// [1. Adım]: BLoC/Cubit Katmanı Oluşturma
 import 'package:bloc/bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 /// Sınıfı Cubit'ten extends et.
-class CounterCubit extends Cubit<int> {
+class CounterCubit extends Cubit<int> with HydratedMixin {
   /// State için başlangıç değeri
   /// super(state) => state(0)
   CounterCubit() : super(0);
@@ -21,4 +22,14 @@ class CounterCubit extends Cubit<int> {
   /// [state] güncellenmesi için
   /// [emit] metodunu kullan
   void decrementCounter() => emit(state - 1);
+
+  /// [Oku/Getir]
+  /// Son [state]'i JSON'dan int'e çevirir
+  @override
+  int? fromJson(Map<String, dynamic> json) => json['value'] as int;
+
+  /// [Yaz/Kaydet]
+  /// Güncellenen [state]'i JSON'a çevirir.
+  @override
+  Map<String, dynamic>? toJson(int state) => {'value': state};
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app.dart';
 import 'counter_observer.dart';
 
 /// [6. Adım]: main metodunu çalıştırma
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   /// Initialize CounterBlocObserver
   /// CounterObserver'ı Başlatma
   Bloc.observer = CounterObserver();
+
+  /// Initialize HydratedStorage
+  /// HydratedStorage'ı Başlatma
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
   runApp(
     const App(),
   );
