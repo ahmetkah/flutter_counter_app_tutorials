7a7ea69aa39cc9994740e11ab38835c0cd05a9ec
“Ahmet <ahmet.kahrimanoglu@fstek.com>
2021-07-10 02:01:54 +0300

    HydratedMixin ile son counter değeri kalıcı hale getirildi

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -2,15 +2,18 @@
 /// [1-C] BLoC Oluşturma
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_event.dart';
 part 'counter_state.dart';
 
-class CounterBloc extends Bloc<CounterEvent, CounterState> {
+class CounterBloc extends Bloc<CounterEvent, CounterState> with HydratedMixin {
   /// State için başlangıç değeri
   /// super(state) => state(0)
-  CounterBloc() : super(CounterState(counter: 0));
+  CounterBloc() : super(CounterState(counter: 0)) {
+    hydrate();
+  }
 
   @override
   Stream<CounterState> mapEventToState(CounterEvent event) async* {
@@ -25,4 +28,14 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
       yield CounterState(counter: state.counter - 1);
     }
   }
+
+  /// [Oku/Getir]
+  /// Son [state]'i JSON'dan CounterState'e çevirir
+  @override
+  CounterState? fromJson(Map<String, dynamic> json) => CounterState(counter: json['value'] as int);
+
+  /// [Yaz/Kaydet]
+  /// Güncellenen [state]'i JSON'a çevirir.
+  @override
+  Map<String, dynamic>? toJson(CounterState state) => {'value': state.counter};
 }
